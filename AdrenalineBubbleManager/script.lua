7fb2f9435e5ba1027db66772a1797a5a362d5227
manwithnoface <anbarsukov@gmail.com>
2017-10-30 12:04:28 +0300

    Add files via upload

--- AdrenalineBubbleManager/script.lua
+++ AdrenalineBubbleManager/script.lua
@@ -13,24 +13,49 @@ game.close()
 color.loadpalette()
 
 buttons.read()
-back = image.load("resources/back.png")
-buttonskey = image.load("resources/buttons.png",20,20)
-buttonskey2 = image.load("resources/buttons2.png",30,20)
-box = image.load("resources/box.png")
 
+-- Set ux0 folder path
 local pathABM = "ux0:data/ABM/"
 files.mkdir(pathABM)
+files.mkdir(pathABM.."lang/")
+files.mkdir(pathABM.."resources/")
 
---font.ttf
-if files.exists(pathABM.."font.ttf") then font.setdefault(pathABM.."font.ttf") end
+-- Loading custom GFX from ux0:data/ABM/resources if exist
+  -- Background image must be (960x554 png or jpg image. Priority to back.png)
+if files.exists(pathABM.."resources/back.png") then back = image.load(pathABM.."resources/back.png")
+elseif files.exists(pathABM.."resources/back.jpg") then back = image.load(pathABM.."resources/back.jpg")
+else
+back = image.load("resources/back.png") end
+  -- Popup message background (must be 706x274 png image)
+if files.exists(pathABM.."resources/box.png") then box = image.load(pathABM.."resources/box.png")
+else
+box = image.load("resources/box.png") end
 
---lang.lua
-if files.exists(pathABM.."lang.lua") then
-	dofile(pathABM.."lang.lua")
+-- Loading default GFX from app folder
+buttonskey = image.load("resources/buttons.png",20,20)
+buttonskey2 = image.load("resources/buttons2.png",30,20)
+
+-- Loading language file
+-- reading system language
+__LANG = os.language()
+-- reading lang strings from ux0:data/ABM/ if exist
+if files.exists(pathABM.."lang/"..__LANG..".txt") then dofile(pathABM.."lang/"..__LANG..".txt")
+else 
+-- reading lang strings fom app folder if exist
+	if files.exists("resources/lang/"..__LANG..".txt") then dofile("resources/lang/"..__LANG..".txt")
 	local cont = 0
 	for key,value in pairs(strings) do cont += 1 end
-	if cont < 40 then files.copy("system/lang.lua",pathABM) dofile("system/lang.lua") end
-else files.copy("system/lang.lua",pathABM) dofile("system/lang.lua") end
+	if cont < 48 then files.copy("resources/lang/english_us.txt",pathABM.."lang/") dofile("resources/lang/english_us.txt") end
+-- reading default lang strings if no one translations founded
+else files.copy("resources/lang/english_us.txt",pathABM.."lang/") dofile("resources/lang/english_us.txt") end
+end
+
+-- Loading custom ttf font if exits
+if files.exists(pathABM.."/resources/"..__LANG..".ttf") then font.setdefault(pathABM.."/resources/"..__LANG..".ttf")
+else 
+ if files.exists("resources/"..__LANG..".ttf") then font.setdefault("resources/"..__LANG..".ttf")
+ end
+end
 
 dofile("system/commons.lua")
 if os.access() == 0 then
@@ -39,6 +64,7 @@ if os.access() == 0 then
 	custom_msg(strings.unsafe,0)
 	os.exit()
 end
+
 
 dofile("git/updater.lua")
 
