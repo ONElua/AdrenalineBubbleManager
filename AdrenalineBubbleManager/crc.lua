1aab623e8ba58c7c87f266d6414eec132434843c
ONELua Team <oneluaproject@gmail.com>
2018-06-30 12:21:52 -0500

    Auto-conversion boot.inf to boot.bin
    Update CRC for new Lman plugins

--- AdrenalineBubbleManager/crc.lua
+++ AdrenalineBubbleManager/crc.lua
@@ -1,22 +1,6 @@
 --CRC for prks
--- default values modules ABM for adrenaline v6.3 commit b234c78:
-_adrbubblebooter_suprx = "0x430C997F"
-_adrenaline_kernel     = "0x12730F40"
-_adrenaline_user       = "0x7B85DDDF"
-_adrenaline_vsh        = "0xA21310E8"
-
-local crc_path = "ux0:data/ABM/crc.ini"
-
-if not files.exists(crc_path) then
-	ini.write(crc_path,"adr_booter","adrbubblebooter_suprx",_adrbubblebooter_suprx)
-	ini.write(crc_path,"adr_kernel","adrenaline_kernel",_adrenaline_kernel)
-	ini.write(crc_path,"adr_user","adrenaline_user",_adrenaline_user)
-	ini.write(crc_path,"adr_vsh","adrenaline_vsh",_adrenaline_vsh)
-end
-
-__CRCADRBOOTER = tonumber(ini.read(crc_path,"adr_booter","adrbubblebooter_suprx",_adrbubblebooter_suprx))
-__CRCKERNEL    = tonumber(ini.read(crc_path,"adr_kernel","adrenaline_kernel",_adrenaline_kernel))
-__CRCUSER      = tonumber(ini.read(crc_path,"adr_user","adrenaline_user",_adrenaline_user))
-__CRCVSH       = tonumber(ini.read(crc_path,"adr_vsh","adrenaline_vsh",_adrenaline_vsh))
-
-
+-- default values modules ABM for adrenaline v6.4
+__CRCADRBOOTER  = 0x7758E511
+__CRCKERNEL     = 0x29569DD9
+__CRCUSER       = 0x1508B8FD
+__CRCVSH        = 0x70167144
--- AdrenalineBubbleManager/script.lua
+++ AdrenalineBubbleManager/script.lua
@@ -19,7 +19,6 @@ dofile("system/callbacks.lua")
 if os.access() == 0 then
 	if back2 then back2:blit(0,0) end
 	screen.flip()
-	--custom_msg(strings.unsafe,0)
 	os.message(strings.unsafe)
 	os.exit()
 end
@@ -90,12 +89,98 @@ if game.exists("PSPEMUCFW") and files.exists(ADRENALINE) and
 			end
 		end
 
+		if oncopy then
+			if back2 then back2:blit(0,0) end
+			screen.flip()
+			os.message(strings.adrinst)
+			os.delay(500)
+		end
+
 	end--__CHECKADR
 
 	if oncopy then
-		--custom_msg(strings.adrinst,0)
-		os.message(strings.adrinst)
-		os.delay(500)
+
+		--Make Bubbles compatible with new boot.bin
+		local list = game.list(__GAME_LIST_APP)
+		table.sort(list ,function (a,b) return string.lower(a.id)<string.lower(b.id) end)
+
+		oncopy = false
+		for i=1, #list do
+			
+			if files.exists(list[i].path.."/data/boot.inf") then
+				if not files.exists(list[i].path.."/data/boot.bin") then
+				
+					if back2 then back2:blit(0,0) end
+					message_wait(strings.upd_bubbles..list[i].id)
+					os.delay(50)
+
+					local path_game = ini.read(list[i].path.."/data/boot.inf", "PATH", "ENABLE")
+
+					---searching game in partitions ux0
+					local partitions = { "ux0:", "uma0:", "ur0:", "imc0:", }
+					local path2game, _find = "", false
+					for j=1, #partitions do
+						path2game = path_game:gsub("ms0:/", partitions[j].."pspemu/")
+						if files.exists(path2game) then _find=true break end
+					end
+
+					local drivers = { "ENABLE", "INFERN0", "MARCH", "NP9660", }--0,0, 1,2
+					local bins = { "ENABLE", "EBOOT.BIN", "EBOOT.OLD", "BOOT.BIN", }--0,0 1,2
+					
+					local driver = ini.read(list[i].path.."/data/boot.inf", "DRIVER", "ENABLE")
+					local bin = ini.read(list[i].path.."/data/boot.inf", "EXECUTE", "ENABLE")
+
+					--Fill boot.bin
+					if _find then
+
+						files.copy("bubbles/pspemuxxx/data/boot.bin",list[i].path.."/data/")
+
+						local fp = io.open(list[i].path.."/data/boot.bin", "r+")
+						if fp then
+							local number = 0
+							
+							--Driver
+							fp:seek("set",0x04)
+							for y=1,#drivers do
+								if driver:upper() == drivers[y] then
+									if y == 1 then number = 0 else number = y - 2 end
+									break
+								end
+							end
+							fp:write(int2str(number))
+
+							--Execute
+							fp:seek("set",0x08)
+							number = 0
+							for y=1,#bins do
+								if bin:upper() == bins[y] then
+									if y == 1 then number = 0 else number = y - 2 end
+									break
+								end
+							end
+							fp:write(int2str(number))
+
+							--Customized
+							fp:seek("set",0x0C)
+							fp:write(int2str(0))
+
+							--Path 2 game
+							fp:seek("set",0x20)
+							local fill = 256 - #path2game
+							for y=1,fill do
+								path2game = path2game..string.char(00)
+							end
+							fp:write(path2game)
+
+							--Close
+							fp:close()
+
+						end--fp
+					end--find
+
+				end
+			end
+		end--for
 		power.restart()
 	end
 
@@ -110,6 +195,5 @@ if game.exists("PSPEMUCFW") and files.exists(ADRENALINE) and
 	scan.show()
 
 else
-	--custom_msg(strings.notadr,0)
 	os.message(strings.notadr)
 end
