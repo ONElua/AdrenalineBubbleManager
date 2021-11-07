3777cb1a43cd709fbaf4c9820c1a6b22ca5351f0
ONELua Team <oneluaproject@gmail.com>
2021-11-07 09:24:59 -0600

    v6.16

--- AdrenalineBubbleManager/git/shared.lua
+++ AdrenalineBubbleManager/git/shared.lua
@@ -4,6 +4,6 @@ APP_REPO = "ONElua"
 APP_PROJECT = "AdrenalineBubbleManager"
 
 APP_VERSION_MAJOR = 0x06 -- major.minor
-APP_VERSION_MINOR = 0x15
+APP_VERSION_MINOR = 0x16
 
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
