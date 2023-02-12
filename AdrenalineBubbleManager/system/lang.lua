9539e9bbc7a8fa9e61c81d0f729f8fa7676627ee
ONELua Team <oneluaproject@gmail.com>
2023-02-12 13:40:38 -0600

    Fix path and new strings

--- AdrenalineBubbleManager/system/lang.lua
+++ AdrenalineBubbleManager/system/lang.lua
@@ -32,16 +32,18 @@ for i=1,#langs do
 	end
 end
 
+__PATH_LANG = "ux0:/data/ABM/lang/"
+files.mkdir(__PATH_LANG)
 function download_langs()
 --local onNetGetFileOld = onNetGetFile; onNetGetFile = nil
 	for i=1,#langs do
 		bubble_id,NResources,TResources = langs[i].file..".txt",i,#langs
 		local res = http.download(string.format("https://raw.githubusercontent.com/%s/%s/master/%s/resources/lang/%s.txt", APP_REPO,APP_PROJECT,APP_PROJECT,
 				string.lower(langs[i].file)), "ux0:data/abm/lang/"..string.lower(langs[i].file..".txt"))
-		if res.headers and res.headers.status_code == 200 and files.exists("ux0:/data/abm/lang/"..langs[i].file..".txt") then
-			files.copy("ux0:/data/abm/lang/"..langs[i].file..".txt","ux0:/ABM/lang/")
+		if res.headers and res.headers.status_code == 200 and files.exists(__PATH_LANG..langs[i].file..".txt") then
+			files.move(__PATH_LANG..langs[i].file..".txt","resources/lang/")
 		else
-			files.delete("ux0:/data/abm/lang/"..langs[i].file..".txt")
+			files.delete(__PATH_LANG..langs[i].file..".txt")
 		end
 		bubble_id = ""
 		NResources, TResources = 0,0
